0c00f29f9a2fcb5bacd1a81b94e9a577d9b403a2
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-01-25 14:13:21 +0100

    logo added

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,16 @@
-import { Link } from "react-router-dom"
-const Logo = ()=>{
-    return(
-        <div className="logo-box">
-        <Link to="/"><img src="../src/Images/logo.png" alt="logo" className=" logo"/></Link>
-           </div>
-    )
-}
+import { Link } from "react-router-dom";
+const Logo = () => {
+  return (
+    <div className="logo-box">
+      <Link to="/">
+        <img
+          src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1706140800&semt=ais"
+          alt="logo"
+          className=" logo"
+        />
+      </Link>
+    </div>
+  );
+};
 
 export default Logo;
